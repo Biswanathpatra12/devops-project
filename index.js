70a78d338e41fac95f1e2fb489fa5c1788974979
EC2 Default User <ec2-user@ip-172-31-34-37.ap-south-1.compute.internal>
2026-05-16 10:53:35 +0000

    devops project

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var port = process.env.PORT || 5000
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get('/', function(req, res) {
+  res.send('🚀 DevOps App Running')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(port, '0.0.0.0', function() {
+  console.log("Server running on port " + port)
 })
