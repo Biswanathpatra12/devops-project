4834c1287e124a1887944be4cfe22f1a139ad3ea
EC2 Default User <ec2-user@ip-172-31-34-37.ap-south-1.compute.internal>
2026-05-17 04:51:56 +0000

    trigger cicd again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 var port = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
-  res.send('🚀 CI CD Check pipeline triggered')
+  res.send('🚀 CI CD pipeline triggered')
 })
 
 app.listen(port, '0.0.0.0', function() {
